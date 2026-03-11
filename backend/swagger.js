7dfc9d7f673c213a9754f86a746b2471373c8a4a
priyaranirai-sdn <your-email@example.com>
2026-03-11 09:40:18 +0530

    update swagger server url

--- backend/swagger.js
+++ backend/swagger.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:5000',
+        url: 'https://priyarani-software-solution.onrender.com',
         description: 'Development server',
       },
     ],
